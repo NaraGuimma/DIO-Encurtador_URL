8beecce39a9db7a9f619dab3733055fc022b19ed
NaraGuimma <naraguimma@gmail.com>
2020-10-10 14:42:00 -0300

    first commit, project DIO-PRO

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+  API_URL: 'http://localhost:5000',
+  MONGO_CONNECTION:
+    'mongodb+srv://new-user:brasil20@cluster0.0ysfi.mongodb.net/Cluster0?retryWrites=true&w=majority',
+};
