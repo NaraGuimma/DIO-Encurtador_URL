ab6b38ba6f122351d9210939b6def5fc95d000d0
Nara Guimaraes <60903424+NaraGuimma@users.noreply.github.com>
2020-10-10 15:18:41 -0300

    Update Constants.ts

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,5 @@
 export const config = {
   API_URL: 'http://localhost:5000',
   MONGO_CONNECTION:
-    'mongodb+srv://new-user:<password>@cluster0.0ysfi.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    'mongodb+srv://<user>:<password>@<database>.yptlq.mongodb.net/url-shortener-dio?retryWrites=true&w=majority',
 };
