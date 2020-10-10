b958b4420e9f12890c8bb239624318c95482fc8f
Nara Guimaraes <60903424+NaraGuimma@users.noreply.github.com>
2020-10-10 14:49:35 -0300

    Update Constants.ts

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,5 @@
 export const config = {
   API_URL: 'http://localhost:5000',
   MONGO_CONNECTION:
-    'mongodb+srv://new-user:brasil20@cluster0.0ysfi.mongodb.net/Cluster0?retryWrites=true&w=majority',
+    'mongodb+srv://new-user:<password>@cluster0.0ysfi.mongodb.net/<dbname>?retryWrites=true&w=majority',
 };
